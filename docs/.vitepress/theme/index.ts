065deb86ea90771d684953b6f75a2cbf51d09893
CJK_mkp <113243675+CJKmkp@users.noreply.github.com>
2025-09-20 17:55:38 +0800

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -12,6 +12,7 @@ import Linkcard from "./components/Linkcard.vue"
 import ArticleMetadata from "./components/ArticleMetadata.vue"
 import HomeUnderline from "./components/HomeUnderline.vue"
 import busuanzi from 'busuanzi.pure.js'
+import DownloadPage from './components/DownloadPage.vue'
 
 export default {
   extends: DefaultTheme,
@@ -26,6 +27,7 @@ export default {
     app.component('Linkcard' , Linkcard);
     app.component('ArticleMetadata' , ArticleMetadata);
     app.component('HomeUnderline' , HomeUnderline);
+    app.component('DownloadPage', DownloadPage);
     if (inBrowser) {
       router.onAfterRouteChanged = () => {
         busuanzi.fetch()
