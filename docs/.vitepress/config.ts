c5036635529dd821cceb26f5829b96b7363805d8
MKStoler1024 <sunguangxuan50@gmail.com>
2026-08-04 15:22:57 +0800

    feat: 国内源加速

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ export default withMermaid(defineConfig({
   base: '/website/',
   head: [
     ['link', { rel: 'icon', href: '/website/images/logo.png' }],
+    ['link', { rel: 'stylesheet', href: 'https://s4.zstatic.net/ajax/libs/font-awesome/6.5.1/css/all.min.css', crossorigin: 'anonymous' }],
   ],
   locales: {
     cn: {
